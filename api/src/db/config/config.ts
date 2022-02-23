f8e82a2a17a2ea1e8f74c8e5e41de900899b396e
Thomas Kuhnert <spielhoelle@posteo.net>
2022-02-24 00:50:00 +0700

    fix: migration needed dialect

--- api/src/db/config/config.ts
+++ api/src/db/config/config.ts
@@ -22,5 +22,12 @@ module.exports = {
     dialect: 'postgres',
     logging: false
   },
-  production: {}
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_TEST_HOST,
+    dialect: 'postgres',
+    logging: false
+  }
 };
